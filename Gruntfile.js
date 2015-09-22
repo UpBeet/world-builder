d9e8052247f0750f17a0d61a9bd9d1ff0ace5663
Peter Gyory <ptg3662@rit.edu>
2015-09-21 21:07:31 -0400

    Setup grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  var testFiles = ['tests/**/*.js'],
+      srcFiles = ['src/**/*.js'],
+      buildFiles = 'build/',
+      jsFiles = srcFiles.concat(testFiles);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Delete all files in the build dir, to ensure clean builds everytime
+    clean: {
+      build: {
+        src: buildFiles
+      }
+    },
+
+    copy: {
+      main: {
+        src: 'src/index.html',
+        dest: 'build/index.html'
+      }
+    },
+
+    // Write node require() code in the browser
+    // Convert ES6 to ES5 before hand with Babel
+    browserify: {
+      options: {
+        transform: [
+          'babelify',
+        ]
+      },
+      dev: {
+        files: {
+          'build/app.js': srcFiles
+        }
+      },
+      prod: {
+        files: {
+          'build/app.js': srcFiles
+        }
+      }
+    },
+
+    watch: {
+      js: {
+        options: { spawn: false, },
+        files: jsFiles,
+        tasks: ['default']
+      }
+    }
+  });
+
+  // Load plugins
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['build']);
+
+  grunt.registerTask('build-dev', ['clean', 'browserify:dev', 'copy']);
+  grunt.registerTask('build-prod', ['clean', 'browserify:prod', 'copy']);
+  grunt.registerTask('build', 'build-dev');
+
+  grunt.registerTask('prod', ['build-prod']);
+};
