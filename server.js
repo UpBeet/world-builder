bdcaf62d44a003fc6c675ddb8290c212a65a8103
Peter Gyory <ptg3662@rit.edu>
2015-09-21 21:03:23 -0400

    Set public directory to build

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 3333));
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
